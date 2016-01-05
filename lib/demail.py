580660fd0afc4a33d613a2f89608f2279cc03823
Choldrim <choldrim@foxmail.com>
2016-01-05 10:43:14 +0800

    first commit

---
+++ lib/demail.py
@@ -0,0 +1,93 @@
+import imghdr
+import os
+import smtplib
+
+from configparser import ConfigParser
+from email.mime.base import MIMEBase
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
+EMAIL_CONF = "%s/.AutoScriptConfig/tower-overtime-reportor/email.ini" % os.getenv("HOME")
+
+FOOTER = """
+<br>
+<br>
+<hr>
+<footer>
+  <p>Posted by: <a href="https://ci.deepin.io">deepin AutomatedScript ci</a></p>
+  <p>Contact: <a href="mailto:tangcaijun@linuxdeepin.com">tangcaijun@linuxdeepin.com</a></p>
+</footer>
+"""
+
+
+class Email:
+    def __init__(self):
+        (server, username, passwd) = self.__get_user_info()
+        self.smtp = self.__smtp(server, username, passwd)
+        self.sender = username
+
+
+    def __smtp(self, server, username, passwd):
+        try:
+            smtp = smtplib.SMTP()
+            smtp.connect(server)
+            smtp.login(username, passwd)
+            smtp.helo()
+            return smtp
+        except Exception as e:
+            print("E: connect mailing service fail")
+            raise e
+
+
+    def __get_user_info(self):
+        config = ConfigParser()
+        config.read(EMAIL_CONF)
+        server = config["USER"]["SMTPServer"]
+        username = config["USER"]["UserName"]
+        passwd = config["USER"]["UserPWD"]
+        return server, username, passwd
+
+
+    def send(self, receiver, subject, content, CC="", files=[], auto_close=False, use_footer=False):
+        """
+        param examples:
+            receiver: a@domain.com,b@domain.com,c@domain.com
+            subject: subject_str
+            content: <h1>hello world</h1>
+            CC: "h@domain.com,i@domain.com"
+            files: ["file_1.jpg", "/home/deepin/file_2.txt", "/tmp/file_3.doc"]
+        """
+        root = MIMEMultipart()
+        root["Subject"] = subject
+        root["From"] = self.sender
+        root["To"] = receiver
+        if CC:
+            root["Cc"] = CC
+
+        if use_footer:
+            content_part = MIMEText(content + FOOTER, "html", "utf-8")
+        else:
+            content_part = MIMEText(content, "html", "utf-8")
+
+        root.attach(content_part)
+
+        if CC:
+            all_receivers = receiver.split(",") + CC.split(",")
+        else:
+            all_receivers = receiver.split(",")
+
+        for send_file in files:
+            att = MIMEText(open(send_file, "rb").read(), "base64", "UTF-8")
+            att["Content-Type"] = "application/octet-stream"
+            att["Content-Disposition"] = 'attachment; filename="%s"' % os.path.basename(send_file)
+            root.attach(att)
+
+        self.smtp.sendmail(self.sender, all_receivers, root.as_string())
+
+        if auto_close:
+            self.close()
+
+
+    def close(self):
+        self.smtp.quit()
+
---
+++ main.py
@@ -0,0 +1,257 @@
+#!/usr/bin/python3
+
+import argparse
+import json
+import operator
+import os
+import re
+import requests
+import time
+
+from datetime import datetime
+from datetime import timedelta
+from configparser import ConfigParser
+
+import xlsxwriter
+from pyvirtualdisplay import Display
+from selenium import webdriver
+
+from lib.demail import Email
+
+
+now = datetime.now()
+last_month_last_date = now.replace(day=1) - timedelta(days=1)
+start_date_str = "%s-%s-01" %(last_month_last_date.year, last_month_last_date.month)
+end_date_str = "%s-%s-%s"  % (last_month_last_date.year, last_month_last_date.month, last_month_last_date.day)
+
+SEND_DAY = 1  # day of every month
+USER_CONF_PATH = "%s/.AutoScriptConfig/tower-overtime-reportor/user.ini" % os.getenv("HOME")
+CAL_URL = "https://tower.im/teams/35e3a49a6e2e40fa919070f0cd9706c8/calendar_events/?start=%s&end=%s" % (start_date_str, end_date_str)
+OVERTIME_CALENDAR_GUID = "b96e5a357a884c7e8c5c2ab12858dd02"
+
+MAIL_RECEIVERS = "wangzhiling@linuxdeepin.com,wangdi@linuxdeepin.com"
+#MAIL_RECEIVERS = "tangcaijun@linuxdeepin.com"
+MAIL_CC = "tangcaijun@linuxdeepin.com"
+
+class ConfigController:
+    def get_login_info(self):
+        config = ConfigParser()
+        config.read(USER_CONF_PATH)
+        username = config["USER"]["UserName"]
+        passwd = config["USER"]["UserPWD"]
+        return username, passwd
+
+    def get_tower_token(self):
+        config = ConfigParser()
+        config.read(USER_CONF_PATH)
+        token = config["USER"]["Token"]
+        return token
+
+
+class BrowserController:
+
+    def __init__(self):
+        self.browser = webdriver.Firefox()
+        self.cc = ConfigController()
+        (username, passwd) = self.cc.get_login_info()
+        self.login(username, passwd)
+
+
+    def login(self, username, passwd):
+
+        print("login to tower ...")
+        login_url = "https://tower.im/users/sign_in"
+        self.browser.get(login_url)
+        unEL = self.browser.find_element_by_id("email")
+        pwdEL = self.browser.find_element_by_name("password")
+        unEL.send_keys(username)
+        pwdEL.send_keys(passwd)
+        unEL.submit()
+
+        time.sleep(5)
+
+        # check login status
+        if self.browser.current_url == "https://tower.im/teams/35e3a49a6e2e40fa919070f0cd9706c8/projects/":
+            print ("login successfully")
+            return True
+
+        else:
+            print ("login error, current url (%s) does not match." % self.browser.current_url)
+            return False
+
+
+    def get_calendar_events(self):
+        self.browser.get(CAL_URL)
+        source = self.browser.page_source
+        el = self.browser.find_element_by_tag_name("body")
+        text = el.text
+        data = json.loads(text)
+        return data
+
+
+class OvertimeAnalyze:
+
+    def __init__(self):
+        self.cc = ConfigController()
+
+    def work(self):
+        #with open("data.json") as fp:
+        #    data = json.load(fp)
+        print("analyzing calendar ...")
+        overtime_datas = self.analyze()
+        month_str = self.get_month_str()
+        file_name = "%s_overtime.xlsx" % (month_str)
+
+        print("writing to excel ...")
+        self.write_excel(overtime_datas, file_name)
+
+        if self.check_send_day():
+            subject = "%s 加班信息统计" % month_str
+            print("sending email ...")
+            self.send_email(subject=subject, files=[file_name])
+
+        print("finish.")
+
+    
+    def get_month_str(self):
+        tmp_str_list = start_date_str.split("-")
+        month_str = "%s-%s" % (tmp_str_list[0], tmp_str_list[1])
+        return month_str
+
+
+    def analyze(self, cal_data=None):
+
+        overtime_datas = []
+
+        if not cal_data:
+            bc = BrowserController()
+            cal_data = bc.get_calendar_events()
+
+        for item in cal_data.get("calendar_events", []).copy():
+            data = {}
+            caleventable_guid = item.get("caleventable_guid")
+            if caleventable_guid != OVERTIME_CALENDAR_GUID:
+                continue
+            guid = item.get("guid")
+
+            nickname = item.get("creator").get("nickname")
+
+            starts_at = item.get("starts_at", "")
+            ends_at = item.get("ends_at", "")
+            time_re = re.compile("(\d+-\d+-\d+)T(\d+:\d+)*")
+            starts_at = " ".join(time_re.findall(starts_at)[0])
+            ends_at = " ".join(time_re.findall(ends_at)[0])
+
+            content = item.get("content", "")
+
+            token = self.cc.get_tower_token()
+            reminders = self.get_reminders(token, guid)
+
+            data["nickname"] = nickname
+            data["starts_at"] = starts_at
+            data["ends_at"] = ends_at
+            data["content"] = content
+            data["reminders"] = reminders
+
+            overtime_datas.append(data)
+
+        return overtime_datas
+
+
+    def get_reminders(self, token, guid):
+        h = {
+                "Authorization":"Bearer %s" % (token, )
+            }
+
+        url = "https://tower.im/api/v2/events/%s" % (guid, )
+
+        r = requests.get(url=url, headers=h)
+        j = r.json()
+        comments = j.get("comments")
+        if len(comments):
+            content = comments[0].get("content")
+            a = re.compile(">@(\w+)<")
+            namelist = a.findall(content)
+            #print(namelist)
+            return " ".join(namelist)
+
+        return ""
+
+
+    def write_excel(self, overtime_datas, file_name="./overtime.xlsx"):
+        COLUMN = {
+                "nickname": "A",
+                "content": "B",
+                "starts_at": "C",
+                "ends_at": "D",
+                "reminders": "E",
+        }
+
+        # sort data
+        overtime_datas = sorted(overtime_datas, key=lambda k: k["starts_at"])
+
+        workbook = xlsxwriter.Workbook(file_name)
+        worksheet = workbook.add_worksheet()
+
+        self.prepare_headers(workbook, worksheet)
+
+        # formats
+        common_format = workbook.add_format()
+        common_format.set_text_wrap()
+        common_format.set_align("top")
+
+        row = 3  # start row
+        for data in overtime_datas:
+            for title, content in data.items():
+                pos = "%s%d" %(COLUMN.get(title), row)
+                worksheet.write(pos, content, common_format)
+            row += 1
+
+
+    def prepare_headers(self, workbook, worksheet):
+
+        # write table header
+        constant = {
+                "B1": "%s 加班信息统计" % (self.get_month_str()),
+                "A2": "姓名",
+                "B2": "内容",
+                "C2": "起始时间",
+                "D2": "结束时间",
+                "E2": "部门负责人",
+                }
+
+        # set content column width
+        worksheet.set_column('A:A', 6) 
+        worksheet.set_column('B:B', 30) 
+        worksheet.set_column('C:E', 15) 
+
+        # format
+        basic_format = workbook.add_format()
+        basic_format.set_align("vcenter")
+        basic_format.set_align("center")
+        basic_format.set_bold()
+
+        # write basic headers
+        for p, content in constant.items():
+            worksheet.write(p, content, basic_format)
+
+    def check_send_day(self):
+        now = datetime.now()
+        if now.day == SEND_DAY:
+            return True
+
+        return False
+
+
+    def send_email(self, subject, files):
+        e = Email()
+        content = ""
+        e.send(MAIL_RECEIVERS, subject, content, CC=MAIL_CC, files=files, auto_close=True, use_footer=True)
+
+
+if __name__ == "__main__":
+    display = Display(visible=0, size=(1366, 768))
+    display.start()
+    oa = OvertimeAnalyze()
+    oa.work()
+    display.stop()
